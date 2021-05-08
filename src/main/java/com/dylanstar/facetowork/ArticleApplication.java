6f760ddfa1d9f929b6d9a364da7a5d79d417b92a
李佳坤 <lijiakun@mmears.com>
2021-05-08 10:15:15 +0800

    add answer

--- src/main/java/com/dylanstar/facetowork/ArticleApplication.java
+++ src/main/java/com/dylanstar/facetowork/ArticleApplication.java
@@ -1,4 +1,4 @@
-package com.dylanstar.facetowork.article;
+package com.dylanstar.facetowork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dylanstar/facetowork/ArticleApplicationTests.java
+++ src/test/java/com/dylanstar/facetowork/ArticleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dylanstar.facetowork.article;
+package com.dylanstar.facetowork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
